c7d516f734f4eefc322b774c038f27bc4af2803c
Ashen Gunaratne <mail@ashenm.ml>
2019-06-22 06:56:34 +0000

    Add file change monitor

---
+++ scripts/watch.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# Watch changes and rebuild artifacts
+
+from time import sleep
+from os import P_WAIT, stat, spawnlp
+
+def monitor(file, settle, cmd):
+
+  # last most modification
+  last = stat(file).st_mtime
+
+  while True:
+
+    # latest modification
+    current = stat(file).st_mtime
+
+    # handle non-changes
+    if last == current:
+      sleep(settle)
+      continue
+
+    last = current
+
+    spawnlp(P_WAIT, *cmd)
+    sleep(settle)
+
+if __name__ == '__main__':
+
+  from os import P_WAIT, spawnl
+  from os.path import basename, join, isfile
+  from sys import argv, exit, stderr
+
+  # resume theme
+  theme = dict(enumerate(argv)).get(1, 'default.xsl')
+  theme_abspath = join('themes', theme)
+
+  # ensure file exsist
+  if not isfile(theme_abspath):
+    print('{}: cannot access \'{}\': No such file or directory'.format(basename(argv[0]), theme_abspath), file=stderr)
+    exit(2)
+
+  # command to execute
+  cmd = ['./scripts/resume.py', './scripts/resume.py', theme]
+
+  # trigger initial build
+  spawnl(P_WAIT, *cmd)
+
+  # monitor file
+  monitor(join('themes', theme), 1, cmd)

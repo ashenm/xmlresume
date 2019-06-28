0280838c905ce1474b2d948c56a66558cb4f4c23
Ashen Gunaratne <mail@ashenm.ml>
2019-06-28 09:04:23 +0000

    Add formation customisation

---
+++ .travis/build.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from yaml import load
+from os import P_WAIT, makedirs, replace, spawnl, spawnv
+from os.path import join
+
+# parse theme formations
+with open('themes.yml', 'rt') as stream:
+  themes = load(stream=stream)
+
+# build all theme formations
+for theme in themes:
+
+  # pdf arguments
+  args = ['scripts/pdf.py']
+
+  # page numbers
+  if themes.get(theme).get('page_numbers'):
+    args.extend(['--page-numbers'])
+
+  # font size
+  if themes.get(theme).get('font_size'):
+    args.extend(['--font-size', str(themes.get(theme).get('font_size'))])
+
+  # font face
+  if themes.get(theme).get('font'):
+    args.extend(['--font', join('fonts', themes.get(theme).get('font'))])
+
+  # log build theme
+  print('\033[36;1mBuilding theme {}.xsl\033[0m'.format(theme))
+
+  # render html document
+  spawnl(P_WAIT, 'scripts/resume.py', 'scripts/resume.py', '{}.xsl'.format(theme))
+
+  # render pdf document
+  spawnv(P_WAIT, 'scripts/pdf.py', args)
+
+  # respective artifacts dir
+  makedirs(name=join('gh-pages', theme), mode=0o700, exist_ok=True)
+
+  # move artifacts
+  replace(src='resume.html', dst=join('gh-pages', theme, 'resume.html'))
+  replace(src='resume.pdf', dst=join('gh-pages', theme, 'resume.pdf'))
--- .travis/build.sh
+++ .travis/build.sh
@@ -5,26 +5,7 @@ set -e
 
 # build all templates
 # into respective directories
-for file in ./themes/*.xsl
-do
-
-  # handle empty dir
-  test -e "$file" || \
-    continue
-
-  # log build theme
-  echo "\033[36;1mBuilding theme $(basename $file)\033[0m"
-
-  # build resume
-  ./scripts/resume.py "$(basename $file)" && ./scripts/pdf.py
-
-  # build artifacts
-  mkdir --parent "gh-pages/$(basename $file .xsl)"
-
-  # move artifacts
-  mv --force "resume.html" "resume.pdf" "gh-pages/$(basename $file .xsl)/"
-
-done
+python3 "$(dirname "$(readlink -f $0)")/build.py"
 
 # webroot default theme artifacts
 cp --force gh-pages/default/resume.html gh-pages/default/resume.pdf gh-pages/
